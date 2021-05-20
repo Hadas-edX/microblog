3d4ee1683164debc8ce8e4567d17aaa7ba9fc2e7
Hadas-edX <68664413+Hadas-edX@users.noreply.github.com>
2021-05-20 15:35:11 +0300

    Add files via upload

---
+++ myapp/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign in')
+
+    
+class PostForm(FlaskForm):
+    choices=['General', 'Python', 'DB', 'Web', 'Storage']
+
+    title = StringField('Title', validators=[DataRequired()])
+    body = StringField('Body')
+    subject = SelectField('Subject', choices=choices)
+    submit = SubmitField('Post')
+    
---
+++ myapp/models.py
@@ -0,0 +1,39 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from myapp import db, login
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(16), index=True, unique=True)
+    first_name = db.Column(db.String(32), index=True)
+    last_name = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(16))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+    def __repr__(self):
+        return f'Post {self.id}'
+
+@login.user_loader
+def user_loader(id):
+    return User.query.get(int(id))
